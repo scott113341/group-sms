e1b2ccda3b13d8804e051fbc4de7bccec87bc76e
Scott Hardy <scott.the.hardy@gmail.com>
2020-01-01 15:19:55 -0700

    Create a trigger function on the "people" table that automatically adds the user to the @all group post-insertion

--- config/schema.sql
+++ config/schema.sql
@@ -4,10 +4,23 @@
 CREATE TABLE people (
     id character varying(32) PRIMARY KEY CHECK (id::text ~ '^@[a-z0-9_]+$'::text),
     name text NOT NULL,
-    number text NOT NULL
+    number text NOT NULL CHECK (number ~ '^\+1\d{10}$'::text)
 );
 
 CREATE UNIQUE INDEX people_pkey ON people(id text_ops);
+
+create function add_to_all()
+returns trigger as $$
+begin
+  INSERT INTO groups VALUES ('@all', NEW.id);
+  return NEW;
+end;
+$$ language plpgsql;
+
+create trigger add_to_all
+after insert on people
+for each row execute procedure add_to_all();
+
 
 ------------
 -- GROUPS --
