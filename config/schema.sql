bfe0d36f0160a70b452d96e893812bbce5e25f01
Scott Hardy <scott.the.hardy@gmail.com>
2020-01-05 19:26:24 -0700

    Add /invite command (#4)
    Main:
    * Add a /invite command to invite brand new users to the group (requires phone number + id + name)
    
    Other:
    * Create a trigger function on the "people" table that automatically adds the user to the @all group post-insertion
    * Add uniqueness constraints to schema

--- config/schema.sql
+++ config/schema.sql
@@ -4,17 +4,32 @@
 CREATE TABLE people (
     id character varying(32) PRIMARY KEY CHECK (id::text ~ '^@[a-z0-9_]+$'::text),
     name text NOT NULL,
-    number text NOT NULL
+    number text NOT NULL CHECK (number ~ '^\+1\d{10}$'::text) UNIQUE
 );
 
 CREATE UNIQUE INDEX people_pkey ON people(id text_ops);
+CREATE UNIQUE INDEX people_number_key ON people(number text_ops);
+
+create function add_to_all()
+returns trigger as $$
+begin
+  INSERT INTO groups VALUES ('@all', NEW.id);
+  return NEW;
+end;
+$$ language plpgsql;
+
+create trigger add_to_all
+after insert on people
+for each row execute procedure add_to_all();
+
 
 ------------
 -- GROUPS --
 ------------
 CREATE TABLE groups (
     group_id character varying(32) NOT NULL CHECK (group_id::text ~ '^@[a-z0-9_]+$'::text),
-    person_id character varying(32) NOT NULL REFERENCES people(id) ON DELETE CASCADE ON UPDATE CASCADE
+    person_id character varying(32) NOT NULL REFERENCES people(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT groups_group_id_person_id_key UNIQUE (group_id, person_id)
 );
 
 CREATE UNIQUE INDEX groups_group_id_person_id_key ON groups(group_id text_ops, person_id text_ops);
